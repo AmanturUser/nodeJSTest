05583577c16c0ea3cd701396558da4895a389382
Sanzhar Akparaliev <taalaibekuulusanzhar@gmail.com>
2024-09-16 16:41:12 +0600

    install mongoDB

--- db.js
+++ db.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose');
 
-//mongodb uri
-const MONGO_URI = "mongodb://localhost:27017/";
+// MongoDB URI with login and password
+const MONGO_URI = "mongodb://admin:admin@localhost:27017/myNewDatabase";
 
-//connect to db
+// Connect to the database
 const connectDb = async () => {
   try {
     await mongoose.connect(MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      authSource: "admin", // Specifies the database that holds the user credentials
     });
-    console.log('Mongoose connected to MONGODB Atlas');
+    console.log('Mongoose connected to MongoDB');
   } catch (err) {
-    console.log(err.message);
+    console.error('Database connection error:', err.message);
     process.exit(1);
   }
 };
