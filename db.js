1ae84eb11b8a4ef655d9303b012af51ab0d3d124
Sanzhar Akparaliev <taalaibekuulusanzhar@gmail.com>
2024-09-16 19:07:31 +0600

    comment mongo url

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
 // MongoDB URI with login and password
-const MONGO_URI = "mongodb://admin:admin@localhost:27017/myNewDatabase";
-
+// const MONGO_URI = "mongodb://admin:admin@localhost:27017/myNewDatabase";
+const MONGO_URI = "mongodb://localhost:27017/";
 // Connect to the database
 const connectDb = async () => {
   try {
     await mongoose.connect(MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      authSource: "admin", // Specifies the database that holds the user credentials
+      // authSource: "admin", 
     });
     console.log('Mongoose connected to MongoDB');
   } catch (err) {
